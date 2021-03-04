a31a31540050e3aeb482abbf6379b2ea4da58a2c
Eduardo Fernandes <edu.fernandes@ua.pt>
2021-03-04 03:29:54 +0000

    add Ex03

---
+++ aula01/Ex03.java
@@ -0,0 +1,30 @@
+import static java.lang.System.*;
+import java.util.Scanner;
+
+public class Ex03 {
+    public static final Scanner in = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int n;
+        while (true) {
+            out.print("N? ");
+            n = in.nextInt();
+            if (n > 0)
+                break;
+            err.println("ERROR: invalid number!");
+        }
+
+        String verb = isPrime(n) ? "is" : "is not";
+        out.printf("Number %d %s prime\n", n, verb);
+    }
+
+    public static boolean isPrime(int n) {
+        // Complete a função para verificar se n é primo...
+        for (int i = 2; i * i < n; i++) {
+            if(n%i==0){
+                return false;
+            }
+        }
+        return true;
+    }
+}
