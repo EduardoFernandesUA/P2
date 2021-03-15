071ebe0224ee9b033083391869a8392ab13f9914
EduardoFernandesUA <72972042+EduardoFernandesUA@users.noreply.github.com>
2021-03-15 11:58:09 +0000

    Update Ex03.java

--- aula01/Ex03.java
+++ aula01/Ex03.java
@@ -20,7 +20,8 @@ public class Ex03 {
 
     public static boolean isPrime(int n) {
         // Complete a função para verificar se n é primo...
-        for (int i = 2; i * i < n; i++) {
+        // referencia: https://www.dcc.fc.up.pt/~pbv/aulas/progimp/teoricas/teorica07.html
+        for (int i = 2; i * i <= n; i++) {
             if(n%i==0){
                 return false;
             }
